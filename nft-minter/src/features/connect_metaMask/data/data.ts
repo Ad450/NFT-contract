143071d3460427ae9951a0ef8acdf2f74bac4b34
Ad450 <adjeimanue@gmail.com>
2022-03-24 02:28:21 +0000

    documenting

--- nft-minter/src/features/connect_metaMask/data/data.ts
+++ nft-minter/src/features/connect_metaMask/data/data.ts
@@ -1,13 +1,9 @@
 /// author : Adjei-Manu Emmanuel
 
 /**  steps
-authentication layer of nft minter
-try to connect to metamask by checking window.ethereum object
-if no object ethereum object is found 
-ask user to install metamask
-if there is an ethereum object, request the list of accounts and get the first one
-the first one is always the current account
-do something when account changes ie. ask user to reconnect to metamask
+
+@function connectMetamask creates a new web3Provider through metamask
+Through this provider, we request user to connect account by requesting metamask accounts
 
  **/
 
@@ -18,20 +14,25 @@ export const fakeVariable = "fake";
 export const connectMetamask = async () => {
   let accounts: [];
   try {
-    // check if browser has metamask injected 
-    if ((window as any).ethereum !== null || (window as any).ethereum !== undefined) {
+    // check if browser has metamask injected
+    if (
+      (window as any).ethereum !== null ||
+      (window as any).ethereum !== undefined
+    ) {
       // creating a new web3Provider through metamask
       // metamask uses infura node under the hood
-      const provider = new ethers.providers.Web3Provider((window as any).ethereum);
+      const provider = new ethers.providers.Web3Provider(
+        (window as any).ethereum
+      );
 
-      accounts = await provider.send("eth_requestAccounts", [])
+      accounts = await provider.send("eth_requestAccounts", []);
 
       console.log(accounts);
-
+    } else {
+      alert("please install metamask to continue using this app");
     }
-
-
   } catch (error) {
-
+    // will do proper error handling
+    console.log(error);
   }
-}
+};
